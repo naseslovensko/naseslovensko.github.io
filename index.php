c704f776a00583b665c45f621f4eaccaebe24ec2
testberg <14354283+testberg@users.noreply.github.com>
2019-06-20 13:20:39 +0200

    Create index.php
    Creating main page

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Naše Slovensko</title>
+<link rel="icon" href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/81/sunflower_1f33b.png">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<style>
+body {
+  background-color: lightpink;
+  text-align: center;
+  color: white;
+  font-family: Arial, Helvetica, sans-serif;
+}
+img{
+	  border-radius: 5px;
+}
+</style>
+</head>
+<body>
+
+<h1>Naše Slovensko</h1>
+<p>&#x2600;&#xfe0f;
+:) &#10084;&#65039;
+&#10084;&#65039;
+&#10084;&#65039;
+&#127803;
+&#10084;&#65039;
+&#10084;&#65039;
+&#10084;&#65039;
+:)
+&#x2600;&#xfe0f;
+</p>
+<img src="http://sabbracadabra.gulas.sme.sk/upload/posts/8/82/82108/large_extra/551744998cb0f415f683a51d30cfc6b4.jpg" alt="sun" style="width:600px">
+
+</body>
+</html>
